a11893bb9c2f5aa167fb45afd8b85f7b852a4edc
prashant pol <prashantn.pol@gmail.com>
2017-02-22 20:47:00 -0500

    Enchancements in product details

--- app/src/main/java/io/quasar/comparisionguru/ProductDetails/PriceFragment.java
+++ app/src/main/java/io/quasar/comparisionguru/ProductDetails/PriceFragment.java
@@ -3,16 +3,28 @@ package io.quasar.comparisionguru.ProductDetails;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import io.quasar.comparisionguru.Model.Product;
 import io.quasar.comparisionguru.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PriceFragment extends Fragment {
+
+    ArrayList<Product> arrayList = new ArrayList<Product>();
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    String[] productdesc, productprice;
 
 
     public PriceFragment() {
@@ -23,8 +35,28 @@ public class PriceFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_price, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_price, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.rv_otherlist);
+        productdesc = getResources().getStringArray(R.array.productdesc);
+        productprice = getResources().getStringArray(R.array.price);
+
+        int i = 0;
+        for (String pname : productdesc) {
+            Product dataprovider = new Product(pname, productprice[i]);
+            arrayList.add(dataprovider);
+            i++;
+        }
+
+        adapter = new ProductRecyclerAdapter(arrayList, getActivity());
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 
 }
---
+++ app/src/main/java/io/quasar/comparisionguru/ProductDetails/ProductRecyclerAdapter.java
@@ -0,0 +1,68 @@
+package io.quasar.comparisionguru.ProductDetails;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.quasar.comparisionguru.Model.Product;
+import io.quasar.comparisionguru.ProductSearchList.SearchListRecyclerAdapter;
+import io.quasar.comparisionguru.R;
+import io.quasar.comparisionguru.SponsoredListRecyclerAdapter;
+
+/**
+ * Created by prashantn.pol on 2017-02-22.
+ */
+
+public class ProductRecyclerAdapter extends RecyclerView.Adapter<ProductRecyclerAdapter.ProductViewHolder> {
+
+    ArrayList<Product> products =new ArrayList<Product>();
+    Context ctx;
+
+    public ProductRecyclerAdapter(ArrayList<Product> products, Context ctx) {
+        this.products = products;
+        this.ctx = ctx;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_card,parent,false);
+        ProductRecyclerAdapter.ProductViewHolder viewHolder=new ProductRecyclerAdapter.ProductViewHolder(view);
+        return viewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        Product product=products.get(position);
+        holder.txt_pname.setText(product.getName());
+        holder.txt_pprice.setText(product.getPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.pname)
+        TextView txt_pname;
+        @BindView(R.id.pprice)
+        TextView txt_pprice;
+
+
+        public ProductViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+}
