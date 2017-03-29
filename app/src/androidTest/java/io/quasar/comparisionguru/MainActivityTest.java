d8278275279b2d620117d94a4778c459a1d47a50
prashant pol <prashantn.pol@gmail.com>
2017-03-29 15:56:02 -0400

    SearchListButtonJunitTesting Added

--- app/src/androidTest/java/io/quasar/comparisionguru/MainActivityTest.java
+++ app/src/androidTest/java/io/quasar/comparisionguru/MainActivityTest.java
@@ -1,5 +1,7 @@
 package io.quasar.comparisionguru;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.support.test.rule.ActivityTestRule;
 import android.view.View;
 
@@ -8,6 +10,12 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import io.quasar.comparisionguru.ProductSearchList.SearchListActivity;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,6 +24,7 @@ import static org.junit.Assert.*;
 public class MainActivityTest {
 
     private  MainActivity mainActivity =null;
+    Instrumentation.ActivityMonitor monitor=getInstrumentation().addMonitor(SearchListActivity.class.getName(),null,false);
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
@@ -30,6 +39,17 @@ public class MainActivityTest {
     {
         View view =mainActivity.findViewById(R.id.rv_sponsoredlist);
         assertNotNull(view);
+
+    }
+
+    @Test
+    public void testLaunchSearchListActivity()
+    {
+        assertNotNull(mainActivity.findViewById(R.id.search));
+        onView(withId(R.id.search)).perform(click());
+        Activity activity= getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+        assertNotNull(activity);
+
     }
 
     @Before
